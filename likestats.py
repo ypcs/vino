ce8d4ba878d3030670e3011e7978744e1854a570
Ville Korhonen <ville@xd.fi>
2012-02-17 17:56:06 +0200

    Replaced tabs with spaces

--- likestats.py
+++ likestats.py
@@ -13,10 +13,10 @@ import sys
 import urllib
 
 try:
-	import json
+    import json
 except ImportError:
-	print "Python >= 2.6 is required for JSON support."
-	sys.exit(1)
+    print "Python >= 2.6 is required for JSON support."
+    sys.exit(1)
 
 
 FB_GRAPH_URL = "https://graph.facebook.com/"
@@ -24,49 +24,49 @@ FB_PAGE_URL = "https://www.facebook.com/pages/"
 FB_BASEURL_LENGTH = 24
 
 def get_page_id(url):
-	if url.startswith(FB_PAGE_URL): # alkaa fb.com/pages, eli sivulla ei ole usernamea
-		p = url.index('/', FB_BASEURL_LENGTH + 7) + 1
-		return url[p:]
-	elif not url.startswith('http'): # ei ole urli vaan sivuid
-		return url
-	else: # muussa tapauksessa sivulla on username, eli muotoa fb.com/jokusivu
-		return url[FB_BASEURL_LENGTH:]
+    if url.startswith(FB_PAGE_URL): # alkaa fb.com/pages, eli sivulla ei ole usernamea
+        p = url.index('/', FB_BASEURL_LENGTH + 7) + 1
+        return url[p:]
+    elif not url.startswith('http'): # ei ole urli vaan sivuid
+        return url
+    else: # muussa tapauksessa sivulla on username, eli muotoa fb.com/jokusivu
+        return url[FB_BASEURL_LENGTH:]
 
 def get_stats(url):
-	graph_url = FB_GRAPH_URL + get_page_id(url)
-	try:
-		data = json.load(urllib.urlopen(graph_url))
-	except:
-		print >>sys.stderr, "Failure while downloading %s" % url
-		data = None
-	return data
-	
+    graph_url = FB_GRAPH_URL + get_page_id(url)
+    try:
+        data = json.load(urllib.urlopen(graph_url))
+    except:
+        print >>sys.stderr, "Failure while downloading %s" % url
+        data = None
+    return data
+    
 
-def main():	
-	if len(sys.argv) != 2:
-		print >>sys.stderr, "Invalid arguments."
-		return 1
-	
-	filename = sys.argv[1]
-	if not os.path.exists(filename):
-		print >>sys.stderr, "File not found: %s" % filename
-		return 1
-	
-	data = open(filename, "r").readlines()
-	
-	for url in data:
-		data = get_stats(url)
-		print '%(id)s,"%(name)s","%(likes)s","%(category)s"' % {
-			'id': data['id'],
-			'name': data['name'],
-			'category': data['category'],
-			'likes': data['likes'],
-		}
-	
-	
-	
-	return 0
+def main():    
+    if len(sys.argv) != 2:
+        print >>sys.stderr, "Invalid arguments."
+        return 1
+    
+    filename = sys.argv[1]
+    if not os.path.exists(filename):
+        print >>sys.stderr, "File not found: %s" % filename
+        return 1
+    
+    data = open(filename, "r").readlines()
+    
+    for url in data:
+        data = get_stats(url)
+        print '%(id)s,"%(name)s","%(likes)s","%(category)s"' % {
+            'id': data['id'],
+            'name': data['name'],
+            'category': data['category'],
+            'likes': data['likes'],
+        }
+    
+    
+    
+    return 0
 
 
 if __name__ == "__main__":
-	sys.exit(main())
+    sys.exit(main())
